2cec2ee03a7771d64adbf7f8f16d2d9ac04b8a96
Teo <teo.calle.lara@gmail.com>
2022-12-13 20:08:14 -0500

    ...

--- QuestionsApp/ViewController.swift
+++ QuestionsApp/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressQuestionLabel: UILabel!
+    
+    let questions = [
+        "¿Swift es un lenguaje de programación desarrollado por Apple?",
+        "¿Siri es un asistente de windows?",
+        "¿Un array puede tener valores duplicados?",
+        "¿La palabra reservada let se usa para definir constantes?"
+    ]
+    
+    var yesButtonPressed = false
+    var notButtonPressed = false
+    var i = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateQuestionLabel()
+        updateProgressQuestionLabel()
     }
 
-
+    private func nextQuestion(){
+        updateQuestionLabel()
+        updateProgressQuestionLabel()
+        updateQuestion()
+    }
+    
+    @IBAction func yesAnswerButtonPressed(_ sender: Any) {
+        nextQuestion()
+        yesButtonPressed = true
+    }
+    
+    @IBAction func notAnswerButtonPressed(_ sender: Any) {
+        nextQuestion()
+        notButtonPressed = true
+    }
+    
+    private func wasAButtonPressed() -> Bool {
+        yesButtonPressed || notButtonPressed
+    }
+    
+    private func updateQuestionLabel(){
+        questionLabel.text = questions[i]
+    }
+    
+    private func updateQuestion(){
+        if i >= questions.count-1 {
+            i = 0
+        } else{
+            i += 1
+        }
+    }
+    
+    private func updateProgressQuestionLabel(){
+        progressQuestionLabel.text = "\(i+1)/10"
+    }
+    
 }
 
